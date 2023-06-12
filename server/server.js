10f7485fdd26d7ae24abcf1c996b7c5e783bf7ab
MrShifu01 <stander.christian.com>
2023-06-12 09:22:11 +0200

    a

--- server/server.js
+++ server/server.js
@@ -19,16 +19,16 @@ app.use(helmet());
 
 // app.use(cors())
 
-// Specify the allowed origin
-const allowedOrigin = 'https://favour-a-tism2-9fjh.vercel.app/'; // Replace with your desired site URL
+// // Specify the allowed origin
+// const allowedOrigin = 'https://favour-a-tism2-9fjh.vercel.app/'; // Replace with your desired site URL
 
-// CORS options
-const corsOptions = {
-  origin: allowedOrigin,
-};
+// // CORS options
+// const corsOptions = {
+//   origin: allowedOrigin,
+// };
 
 // Enable CORS for all routes
-app.use(cors(corsOptions));
+app.use(cors({origin: ['https://favour-a-tism2-9fjh.vercel.app/', 'http://localhost:3000']}));
 
 // Set the port to either the value specified in the environment variable "PORT" or 8000
 const PORT = process.env.PORT || 8000;
